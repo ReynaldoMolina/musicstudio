221e9f9ced10266a4877d2af5580f09d6e4cf960
ReynaldoMolina <molinareynaldo952@gmail.com>
2025-02-06 14:12:39 -0600

    Slideshow now has smoother animation, rounded arrow icons. Added background color to show while page loads

--- assets/script.js
+++ assets/script.js
@@ -4,35 +4,40 @@
     3. Si se deja sin presionar por un tiempo, ejecutar slideShow
 */
 
-var slideIndex = 0;
-var slideIndexPrevious = 0;
-var lastPressed = 1;
-var slideContinue = true;
-var slides = document.getElementsByClassName("slides");
+let slideIndex = 0;
+let slideIndexPrevious = 0;
+let lastPressed = 1;
+let slideInterval;
+let slideTimeOut;
+let fadeType;
+const slides = document.getElementsByClassName("slides");
 
 function moveDivs(n) {
+    clearTimeout(slideTimeOut);
+    clearInterval(slideInterval);
     lastPressed = n;
     slideIndex += n;
+    fadeType = "fade-in-img-fast";
     slideShow(slideIndex);
+    slideTimeOut = setTimeout(checkInterval, 6000, 1);
 }
 
-function moveDivs(n) {
+function autoMoveDivs(n) {
     lastPressed = n;
     slideIndex += n;
+    fadeType = "fade-in-img";
     slideShow(slideIndex);
 }
 
 function checkInterval(n) {
-    //check if slideContinue
-    if (slideContinue) {
-        setInterval(moveDivs, 4000, n);
-    }
+    slideInterval = setInterval(autoMoveDivs, 4000, n);
 }
 
 function slideShow(index) {
     // hide all images
     for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove('fade-in-img')
+        slides[i].classList.remove("fade-in-img");
+        slides[i].classList.remove("fade-in-img-fast");
         slides[i].style.display = "none";
     }
 
@@ -45,7 +50,7 @@ function slideShow(index) {
         slideIndex = slides.length - 1;
     }
 
-    //set previous slide
+    // set previous slide
     if (lastPressed > 0) { //if right button pressed
         slideIndexPrevious = slideIndex - 1;
         if (slideIndexPrevious < 0) {
@@ -61,10 +66,10 @@ function slideShow(index) {
 
     // make images visible
     slides[slideIndex].style.display = "block";
-    slides[slideIndex].classList.add("fade-in-img");
+    slides[slideIndex].classList.add(fadeType);
     slides[slideIndexPrevious].style.display = "block";
 }
 
-slides[0].classList.add("fade-in-img");
-slides[0].style.display = "block";
+slides[slideIndex].style.display = "block";
+slides[slideIndex].classList.add("fade-in-img");
 checkInterval(1);
